f4c34dd82ffecb8e5d6136880e05a791bd265471
pedro_silva <pedrohsilva0407@outlook.com>
2025-06-03 17:16:41 -0300

    Adicionando interpretador, biblioteca e iniciando conexão com esp32

---
+++ ProbabilidadeWithEsp.py
@@ -0,0 +1,17 @@
+import asyncio
+import websockets
+
+async def handle_esp_32(websocket):
+    print("Esp32 conectado com sucesso")
+    try:
+        async for message in websocket:
+            print(f"Recebido de esp32: {message}")
+            await websocket.send("mensagem recebida com sucesso")
+    except websockets.ConnectionClosed:
+        print("Esp32 desconectado")
+
+async def main():
+    async with websockets.serve(handle_esp_32, "192.168.0.5", 8765):
+        await asyncio.Future()
+
+asyncio.run(main())
